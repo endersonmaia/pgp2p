d3589010099db75c24799c00a014981837740c5a
Enderson Maia <endersonmaia@gmail.com>
2011-02-02 15:24:53 -0300

    * Removing "[" and "]" from track.toString() now in the correct place.

--- src/net/pgp2p/networkhandler/PGP2PMessage.java
+++ src/net/pgp2p/networkhandler/PGP2PMessage.java
@@ -75,8 +75,6 @@ public class PGP2PMessage extends Message {
 		if (message.getMessageElement(NAMESPACE, TRACK_FIELD) != null ) {
 			String element = message.getMessageElement(NAMESPACE, TRACK_FIELD).toString();
 			
-			element = element.replace("[", "").replace("]", "");
-			
 			track.clear();
 			track.addAll(Arrays.asList( element.split( ", " ) ));
 		}
@@ -185,7 +183,7 @@ public class PGP2PMessage extends Message {
 	public PGP2PMessage addTrack(List<String> tracks) {
 		this.track.addAll(tracks);
 		
-		MessageElement elemTrack = new StringMessageElement(TRACK_FIELD, track.toString(), null);
+		MessageElement elemTrack = new StringMessageElement(TRACK_FIELD, track.toString().replace("[", "").replace("]", ""), null);
 		replaceMessageElement(NAMESPACE, elemTrack);
 		
 		return this;
